1393524d18be64ca4bc0e08513ffb2a508a884b4
Alina Wang <alinawang1125@gmail.com>
2016-12-15 21:30:37 +0800

    查看与我相关任务

--- routes/issues.js
+++ routes/issues.js
@@ -4,6 +4,33 @@ var config = require('../config');
 var apiformat = require('../apiformat');
 var request = require('request');
 
+router.route("/mine")
+.get(function(req,res,next){
+    var statusCode = 200;
+    var success = true;
+    var data = {};
+    var message = 'Get Issues List Success';
+
+    var opts = config.buildOptions("todos", "GET", false, req.get('PRIVATE-TOKEN'));
+    opts.body = JSON.stringify(req.body);
+
+    request(opts, function (error, response, body) {
+        statusCode = response.statusCode;
+        if (!error && statusCode==200) {
+            var info = JSON.parse(body);
+            data = info;
+        }
+        else {
+            success = false;
+            var errInfo = JSON.parse(body);
+            message = errInfo.message;
+            console.log('something wrong! '+ message);
+        }
+        var formattedResponse = apiformat.formatResponse(statusCode,message,data,success);
+        res.send(formattedResponse);
+    })
+})
+
 /* GET issues listing. */
 router.route("/")
 .get(function (req, res, next) {
